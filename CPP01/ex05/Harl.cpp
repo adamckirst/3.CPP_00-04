41d74ec65aeb93b99792c41d9fa871a984a85549
Adam Chien Kirst <achien-k@c2r4s7.42porto.com>
2024-08-28 12:29:14 +0100

    CPP01 V1

--- CPP01/ex05/Harl.cpp
+++ CPP01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: achien-k <achien-k@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 17:55:38 by achien-k          #+#    #+#             */
-/*   Updated: 2024/08/27 16:55:38 by achien-k         ###   ########.fr       */
+/*   Updated: 2024/08/28 12:25:32 by achien-k         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,25 @@
 
 void Harl::debug(void)
 {
-	std::cout << "Uh, Rick, I really think that portal gun is, like, the coolest thing ever. \
-	I mean, I love watching you tinker with it. It's so, uh, fascinating!"
+	std::cout << "Uh, Rick, I really think that portal gun is, like, the coolest thing ever. I mean, I love watching you tinker with it. It's so, uh, fascinating!"
 			  << std::endl;
 }
 
 void Harl::info(void)
 {
-	std::cout << "Geez, Rick, every time we go on these crazy adventures, \
-	I end up covered in some alien goo or another. It’s like, can’t we have just \
-	one normal day without almost dying?"
+	std::cout << "Geez, Rick, every time we go on these crazy adventures, I end up covered in some alien goo or another. It’s like, can’t we have just one normal day without almost dying?"
 			  << std::endl;
 }
 
 void Harl::warning(void)
 {
-	std::cout << "I don’t know, Rick... I mean, messing with the time stream seems like a \
-	really bad idea. What if we end up, like, creating a paradox or something?"
+	std::cout << "I don’t know, Rick... I mean, messing with the time stream seems like a really bad idea. What if we end up, like, creating a paradox or something?"
 			  << std::endl;
 }
 
 void Harl::error(void)
 {
-	std::cout << "Oh man, Rick, this is bad! Really bad! We’re totally screwed! \
-	We gotta get out of here before everything blows up, or—or we get vaporized, or something !"
+	std::cout << "Oh man, Rick, this is bad! Really bad! We’re totally screwed! We gotta get out of here before everything blows up, or—or we get vaporized, or something !"
 			  << std::endl;
 }
 
